e45fc634b2283438260e1e45f6d75677f0301b8e
Abdellah Ramadan <ajrmdn@gmail.com>
2019-07-19 14:30:40 -0700

    add a third spec and test.

--- lib/anagrams_antigrams.rb
+++ lib/anagrams_antigrams.rb
@@ -1,23 +1,24 @@
 require ('pry')
-require ('rspec')
+# require ('rspec')
 
 class String
   define_method(:anagrams) do
     input_array = self.downcase().split(",")
-    word_one = (word_array[0]).split("")
-    word_two = (word_array[1]).split("")
+    word_one = (word_array[0]).split(" ")
+    word_two = (word_array[1]).split(" ")
 
     word_one_split = word_one.sort()
     word_two_split = word_two.sort()
 
-    input_test = word_one.join("") + word_two.join("")
+    input_test = word_one.join(" ") + word_two.join(" ")
     if (word_one & word_two).empty?
       anagram_outcome = "This is not an Anagram. This is an Antigram"
     else
       if input_test.scan(/[aeiou]/).count > 0
-        if (word_one_split.join("") == word_two_split.join(""))
+        if (word_one_split.join(" ") == word_two_split.join(" "))
           anagram_outcome = "This is an Anagram."
-        end
+        
+      end
 
 
   end
--- spec/anagrams_antigrams_spec.rb
+++ spec/anagrams_antigrams_spec.rb
@@ -5,8 +5,14 @@ describe('String#anagrams') do
   it('tests two words to see if they are anagrams') do
     expect("ruby, bury".anagrams()).to(eq("This is an Anagram"))
   end
+
   it('tests two words to see if they are anagrams or not') do
-  expect("ruby, bry".anagrams()).to(eq("This is not an Anagram"))
-end
+    expect("ruby, bry".anagrams()).to(eq("This is not an Anagram"))
+  end
+
+  it('tests to see if the user input is an anagram') do
+    expect("this,that".anagram()).to(eq("This is Not an Anagram. This is an Antigram"))
+  end
+
 
 end
