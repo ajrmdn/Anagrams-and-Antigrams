c5adabdb719e1fc1884568eefab769fd093ad7cc
Abdellah Ramadan <ajrmdn@gmail.com>
2019-07-19 14:02:17 -0700

    research ruby debugging.

--- spec/anagrams_antigrams_spec.rb
+++ spec/anagrams_antigrams_spec.rb
@@ -5,7 +5,7 @@ describe('String#anagrams') do
   it('tests two words to see if they are anagrams') do
     expect("ruby, bury".anagrams()).to(eq("This is an Anagram"))
   end
-  it('tests two words to see if they are anagrams') do
+  it('tests two words to see if they are anagrams or not') do
   expect("ruby, bry".anagrams()).to(eq("This is not an Anagram"))
 end
 
