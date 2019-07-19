487b8937c223b153e062fad3f5562dc2061874c7
Abdellah Ramadan <ajrmdn@gmail.com>
2019-07-19 13:30:15 -0700

    add a second spec and test.

--- spec/anagrams_antigrams_spec.rb
+++ spec/anagrams_antigrams_spec.rb
@@ -5,5 +5,8 @@ describe('String#anagrams') do
   it('tests two words to see if they are anagrams') do
     expect("ruby, bury".anagrams()).to(eq("This is an Anagram"))
   end
+  it('tests two words to see if they are anagrams') do
+  expect("ruby, bry".anagrams()).to(eq("This is not an Anagram"))
+end
 
 end
