3b7f5635a8b56ffff402c58e2a075a03e758a8cd
Abdellah Ramadan <ajrmdn@gmail.com>
2019-07-19 11:30:33 -0700

    continue to debug. my first spec passed but now its not passing.

--- lib/anagrams_antigrams.rb
+++ lib/anagrams_antigrams.rb
@@ -4,17 +4,26 @@ require ('rspec')
 class String
   define_method(:anagrams) do
     input_array = self.downcase().split(",")
-    # word_one = (word_array[0]).split("")
-    # word_two = (word_array[1]).split("")
-    #
-    # word_one_checked = word_one.sort()
-    # word_two_checked = word_two.sort()
+    word_one = (word_array[0]).split("")
+    word_two = (word_array[1]).split("")
 
+    word_one_split = word_one.sort()
+    word_two_split = word_two.sort()
 
-    end
+    input_test = word_one.join("") + word_two.join("")
+    if (word_one & word_two).empty?
+      anagram_outcome = "This is not an Anagram. This is an Antigram"
+    else
+      if input_test.scan(/[aeiou]/).count > 0
+        if (word_one_split.join("") == word_two_split.join(""))
+          anagram_outcome = "This is an Anagram."
+        end
+      
+
+  end
 end
 
 
-puts "Please enter two words:"
+p "Please enter two words:"
 input = gets.to_str
 input.anagrams
--- spec/anagrams_antigrams_spec.rb
+++ spec/anagrams_antigrams_spec.rb
@@ -2,7 +2,7 @@ require ('rspec')
 require ('anagrams_antigrams.rb')
 
 describe('String#anagrams') do
-  it("tests two words to see if they are anagrams") do
+  it('tests two words to see if they are anagrams') do
     expect("ruby, bury".anagrams()).to(eq("This is an Anagram"))
   end
 
