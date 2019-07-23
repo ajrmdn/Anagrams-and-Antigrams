64c29f61dfa4a4fff283e87b9102fa52b7e48030
Abdellah Ramadan <ajrmdn@gmail.com>
2019-07-23 13:19:28 -0700

    DRY up refactor code.

--- lib/anagrams_antigrams.rb
+++ lib/anagrams_antigrams.rb
@@ -4,41 +4,28 @@ class Word
   attr_accessor(:word_one, :word_two)
 
   def initialize(word_one, word_two)
-      @word_one = word_one
-      @word_two = word_two
+      @word_one = word_one.downcase.gsub(/[\s+,.?!#@"']/, '').split("").sort
+      @word_two = word_two.downcase.gsub(/[\s+,.?!#@"']/, '').split("").sort
   end
 
-  def anagram(word_one, word_two)
-    word_one_array = word_one.downcase.gsub(/[\s+,.?!#@"']/, '').split("").sort
-    word_two_array = word_two.downcase.gsub(/[\s+,.?!#@"']/, '').split("").sort
-
-    if ((word_one_array.length === 0) | (word_two_array.length === 0))
+  def anagram()
+    
+    if ((@word_one.length == 0) || (@word_two.length == 0))
       "Please enter both words"
-    elsif word_one_array === word_two_array
+    elsif @word_one == @word_two
       "Thats an anagram!"
     else
       "This is not an anagram."
     end
   end
 
-  def antigram(word_one, word_two)
-    word_one_array = word_one.downcase.gsub(/[\s+,.?!#@"']/, '').split("")
-    word_two_array = word_two.downcase.gsub(/[\s+,.?!#@"']/, '').split("")
+  def antigram()
     is_antigram = true
-    word_one_array.each do |letter|
-      if ((is_antigram == true) & (word_two_array.include?(letter)))
+    @word_one.each do |letter|
+      if ((is_antigram == true) & (@word_two.include?(letter)))
         is_antigram = false
       end
     end
     is_antigram
   end
-
 end
-
-
-
-
-
-# p "Please enter two words:"
-# input = gets.chomp
-# input.anagrams
--- spec/anagrams_antigrams_spec.rb
+++ spec/anagrams_antigrams_spec.rb
@@ -2,37 +2,39 @@ require ('rspec')
 require ('anagrams_antigrams')
 
 describe('#anagrams') do
-  word = Word.new("ruby", "bury")
 
   it('tests whether a word regardless of capitalization was entered or not') do
-    expect(word.anagram("ruby", "")).to(eq("Please enter both words"))
+    word = Word.new("ruby", "")
+    expect(word.anagram()).to(eq("Please enter both words"))
   end
 
   it('tests two words regardless of capitalization to see if they are anagrams') do
-    expect(word.anagram("ruby", "bury")).to(eq("Thats an anagram!"))
+    word = Word.new("ruby", "bury")
+    expect(word.anagram()).to(eq("Thats an anagram!"))
   end
 
   it('tests two words regardless of capitalization to see if they arent anagrams') do
-    expect(word.anagram("ruby", "dig")).to(eq("This is not an anagram."))
+    word = Word.new("ruby", "dig")
+    expect(word.anagram()).to(eq("This is not an anagram."))
   end
 
   it('tests two words regardless of capitalization to see if they are antigram') do
-    expect(word.antigram("taco", "fish")).to(eq(true))
+    word = Word.new("taco", "fish")
+    expect(word.antigram()).to(eq(true))
   end
 
   it('tests two words regardless of capitalization to see if they are not an antigram') do
-    expect(word.antigram("dish", "fish")).to(eq(false))
+    word = Word.new("dish", "fish")
+    expect(word.antigram()).to(eq(false))
   end
 
   it('tests multiple words regardless of capitalization to see if they are an anagram') do
-    expect(word.anagram("Ronald Reagan", "a darn long era")).to(eq("Thats an anagram!"))
+    word = Word.new("Ronald Reagan", "a darn long era")
+    expect(word.anagram()).to(eq("Thats an anagram!"))
   end
 
   it('tests multiple words regardless of capitalization to see if they are an antigram') do
-    expect(word.antigram("Ronald Reagan", "kit")).to(eq(true))
+    word = Word.new("Ronald Reagan", "kit")
+    expect(word.antigram()).to(eq(true))
   end
-
-
-
-
 end
